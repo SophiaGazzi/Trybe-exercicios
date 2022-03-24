342f2c0da8872ea143b4c3c49ab54203055a8bb6
Sophia <sophiamemy@hotmail.com>
2022-03-24 19:33:57 -0300

    Adiciona os exercicios

---
+++ bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,13 @@
+let form = document.querySelector('.speaker-form');
+let buttonSend = document.querySelector('#botão');
+
+function send (event){
+
+    event.presentDefaul();
+
+    console.log(form.value);
+    form.innerHTML = form.value;
+
+}
+
+buttonSend.addEventListener('click',send);
